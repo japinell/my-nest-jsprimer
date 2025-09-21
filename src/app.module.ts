3936468eb40424f7e3ecb4d8c4e65b16ce8f115e
Jose A. Pinell <japinell@yahoo.com>
2025-09-21 14:41:05 -0700

    Wired products to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+
 import { ProductsController } from "./products/products.controller";
 import { ProductsService } from "./products/products.service";
 import { ProductsModule } from "./products/products.module";
