7dedbdd499b027bcae2b724d189215ba29554713
Jose A. Pinell <japinell@yahoo.com>
2025-09-21 14:26:50 -0700

    Added the products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
